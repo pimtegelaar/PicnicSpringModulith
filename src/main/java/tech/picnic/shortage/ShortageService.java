80ce146b1e68bddacfe7325f9379346f76301369
Pim Tegelaar <Pim.Tegelaar@teampicnic.com>
2024-05-14 23:56:33 +0200

    One minute

--- src/main/java/tech/picnic/shortage/ShortageService.java
+++ src/main/java/tech/picnic/shortage/ShortageService.java
@@ -1,13 +1,13 @@
 package tech.picnic.shortage;
 
-import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import tech.picnic.picking.ShortageEvent;
 
 @Service
 public class ShortageService {
 
-  @ApplicationModuleListener
+  @EventListener
   public void notifyShortage(ShortageEvent event) throws InterruptedException {
     System.out.println("Notifying shortage of article " + event.articleId());
     Thread.sleep(1000);
